fa7f0e13e418d6b3fff1fe00fc21ff19f3990589
eraylee <599023328@qq.com>
2020-08-17 17:49:53 +0800

    fix validate

--- src/app/auth/strategys/local.strategy.ts
+++ src/app/auth/strategys/local.strategy.ts
@@ -1,6 +1,6 @@
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { AuthService } from '../auth.service';
 import { JwtPayload } from '../jwt-payload.interface';
 
@@ -16,7 +16,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(username: string, password: string): Promise<JwtPayload> {
     const user = await this.authService.validateUser({ username, password });
     if (!user) {
-      throw new UnauthorizedException('用户名或密码错误');
+      throw new BadRequestException('用户名或密码错误');
     }
     return user;
   }
