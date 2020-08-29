8c7d888e16b0e90eaad2002b9ba4e4326d50d53b
eraylee <599023328@qq.com>
2020-08-29 18:22:55 +0800

    完善 登录日志

--- src/app/log/login-info/login-info.controller.ts
+++ src/app/log/login-info/login-info.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { LoginInfoService } from './login-info.service';
 import { ApiOperation, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { LoginInfo } from './login-info.schema';
+import { PaginationResult } from '@/interfaces/result.interface';
+import { PaginationDto } from '@/common/base/base.dto';
 
 @Controller()
 @ApiTags('loginInfo')
@@ -9,11 +11,31 @@ import { LoginInfo } from './login-info.schema';
 export class LoginInfoController {
   constructor(private service: LoginInfoService) {}
   /**
+   * 通过id查询
+   * @param id
+   */
+  @ApiOperation({ summary: '通过id查询' })
+  @Get('queryById')
+  async queryById(@Query('id') id: string): Promise<LoginInfo> {
+    return await this.service.findById(id);
+  }
+  /**
    * 查询全部
    */
   @ApiOperation({ summary: '查询全部' })
-  @Get('queryById')
+  @Get('queryAll')
   async queryByAll(): Promise<LoginInfo[]> {
     return await this.service.findAll();
+  }
+  /**
+   * 分页查询
+   * @param params
+   */
+  @ApiOperation({ summary: '分页查询' })
+  @Get('queryPage')
+  async queryPage(
+    @Query() params: PaginationDto,
+  ): Promise<PaginationResult<LoginInfo>> {
+    return await this.service.queryPage(params);
   }
 }
--- src/app/log/login-info/login-info.service.ts
+++ src/app/log/login-info/login-info.service.ts
@@ -3,11 +3,18 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { LoginInfo } from './login-info.schema';
 import { UAParser } from 'ua-parser-js';
+import { PaginationResult } from '@/interfaces/result.interface';
+import { PaginationDto } from '@/common/base/base.dto';
+import { OrderTypes } from '@/enums';
 
 @Injectable()
 export class LoginInfoService {
   constructor(@InjectModel(LoginInfo.name) private model: Model<LoginInfo>) {}
-
+  /**
+   * 新增
+   * @param params
+   * @param agent
+   */
   async create(params: Partial<LoginInfo>, agent: string): Promise<LoginInfo> {
     const uaParser = new UAParser(agent);
     const os = uaParser.getOS();
@@ -15,8 +22,47 @@ export class LoginInfoService {
     const createdCat = new this.model({ ...params, os, browser });
     return await createdCat.save();
   }
-
+  /**
+   * 查询全部
+   */
   async findAll(): Promise<LoginInfo[]> {
     return await this.model.find().exec();
+  }
+  /**
+   * 通过id查询
+   * @param id
+   */
+  async findById(id: string): Promise<LoginInfo> {
+    return await this.model.findById(id);
+  }
+
+  async queryPage({
+    pageSize,
+    pageNum,
+    orderType,
+    orderColumn,
+  }: PaginationDto): Promise<PaginationResult<LoginInfo>> {
+    const take = pageSize ?? 10;
+    const page = pageNum ?? 1;
+    const skip = take * (page - 1);
+    const _orderType = orderType ?? OrderTypes.DESC;
+    const _orderColumn = orderColumn ?? 'createdAt';
+    const order: any = {
+      [_orderColumn]: _orderType,
+    };
+    const total = await this.model.count({}).exec();
+    const data = await this.model
+      .find()
+      .skip(skip)
+      .limit(take)
+      .sort(order)
+      .exec();
+    return {
+      data,
+      total,
+      pageNum: page,
+      pageSize: take,
+      maxPage: Math.ceil(total / take),
+    };
   }
 }
